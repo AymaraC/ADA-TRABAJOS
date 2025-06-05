a87fe3ecb2d8dec38a79dff1cba52792102e20fd
AymaraCabanl <aymicabanal@hotmail.com>
2025-06-05 00:03:56 -0300

    Modifico ejercicio 5

--- clase9/ejercicio5.js
+++ clase9/ejercicio5.js
@@ -8,8 +8,10 @@ let numero3 = parseInt (promptsync("Ingrese el tercer número: "))
 
     if ((numero1 > numero2) && (numero3 < numero1)) {
         console.log ("El primer número es el mayor")
+
     }else if (numero2 > numero1 && numero3 < numero2){
         console.log ("El segundo número es el mayor")
+    
     } else 
         console.log ("El tercer número es el mayor")
         
