10ec85674328b4479f8c198a29f9d3d009748ee4
AymaraCabanl <aymicabanal@hotmail.com>
2025-07-04 20:52:47 -0300

    Reubicados archivos de la clase 15 en su carpeta correspondiente

---
+++ clase15/ejercicio9.js
@@ -0,0 +1,16 @@
+/*Ejercicio 9: Eliminar espacios innecesarios 
+Tienes la siguiente cadena con espacios adicionales  
+"    Hola, mundo!    " 
+Elimina los espacios al inicio y al final y muestra la cadena resultante. */
+
+let frase = " ¡Hola, mundo! "
+
+fraseCorregida = frase.trim()
+
+console.log(fraseCorregida)
+
+
+
+
+
+
