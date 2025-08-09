e7b14222e1c901e2041fbde9833a86d22afc89cd
AymaraCabanl <aymicabanal@hotmail.com>
2025-08-09 13:22:51 -0300

    Agrego ejercicio clase 5

--- BACK-END.MODULOII/clase5/servidor.js
+++ BACK-END.MODULOII/clase5/servidor.js
@@ -1,62 +1,39 @@
-/*Crea un archivo llamado servidor.js. 
-2. Importa el módulo que te permite trabajar con servidores TCP 
-(net). 
+/*1. Crea un archivo llamado servidor.js. 
+2. Importa el módulo que te permite trabajar con servidores TCP (net). 
 3. Usa el método que crea un servidor. 
 Pista: Este método necesita una función que se ejecuta 
 cuando alguien se conecta. 
 4. Configura el servidor para que escuche en el puerto 5000. 
 Pista: Usa el método que pone al servidor "a escuchar". 
-5. Agrega un mensaje en la consola para confirmar que el servidor 
-está listo.*/
+5. Agrega un mensaje en la consola para confirmar que el servidor está listo.*/
 
-const net = require ('net')
+const net = require ('net');
 
-const PORT = 5000 //crear variable para darle flexibilidad y solo cambiar el numero del puerto 
+const PORT = 5000 //crear variable para darle flexibilidad y solo cambiar el numero del puerto.
 
-server.listen(PORT, () => {
-    console.log('Cliente escuchando en el puerto ', PORT)
-})
-
-/*Objetivo: Mostrar en la consola cuando un cliente se conecta al servidor. 
-Guía paso a paso: 
-1. Revisa la función que agregaste al crear el servidor. 
-o Pista: Esa función se ejecuta cada vez que un cliente se 
-conecta. 
-2. Dentro de esa función, agrega un mensaje en la consola que diga: 
-"¡Un cliente se ha conectado!". 
-3. Verifica que el mensaje aparezca cuando el servidor recibe una 
-conexión. */
-
-const servidor = net.createServer((socket) => {
+const server = net.createServer((socket) => {     //Ejercicio 2: creamos el servidor TCP utilizando server y mostramos con el console que el cliente se conectó.
     console.log('¡Un cliente se ha conectado!')
-})
-
-/*Objetivo: Mostrar en la consola cualquier mensaje que el cliente envíe al 
-servidor. 
-Guía paso a paso: 
-1. Usa el evento data para detectar cuando el cliente envía datos. 
-Pista: El evento se llama en el socket que representa la 
-conexión del cliente. 
-2. En el callback del evento, convierte los datos recibidos (buffer) en 
-texto usando .toString(). 
-3. Muestra el mensaje en la consola con un texto como: 
-"Mensaje recibido del cliente: <mensaje>".*/
-
-socket.on('data', (data)=>{
-    const mensaje = data.toString() //guardo en una constante la data que entra en un formato string para que sea legible
+    socket.on('data', (data)=>{
+    const mensaje = data.toString() //guardo en una constante la data que entra en un formato string para que sea legible 
+    // porque el servidor recibe los datos del cliente en forma de buffer(sistema binario). Este es el ejercicio 3.
     console.log('Mensaje recibido del cliente: ', mensaje)
-
-//Ejercicio 4, se envia al cliente una respuesta.
-    const respuesta = 'Mensaje recibido, correctamente.'
+//Ejercicio 4: Responder al cliente
+    const respuesta = '¡Hola Cliente!'
     socket.write(respuesta)
+        });
 
-})
+//Ejercicio 5: Detectar cuando el cliente se desconecta 
 
-//Ejercicio 5, mostrar mensaje de finalización.
-    socket.on('end',()=>{
-        console.log('Cliente desconectado.')
+    socket.on('end',() => {
+        console.log('Cliente desconectado.');
+        
     })
 
+});
+
+server.listen(PORT, () => {         //para que el servidor escuche en el puerto definido
+    console.log('Cliente escuchando en el puerto ', PORT)
+});
 
 
 
